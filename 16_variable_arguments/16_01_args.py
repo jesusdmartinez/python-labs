b42ebf410a5eded48c21eea3788e450547520fab
Jesus Martinez <jesusd.martinez@Jesuss-MBP.home>
2019-09-19 10:13:27 -0700

    16_Args and Kwargs

--- 16_variable_arguments/16_01_args.py
+++ 16_variable_arguments/16_01_args.py
@@ -5,6 +5,6 @@ Write a script with a function that demonstrates the use of *args.
 
 def make_pizza(*args):
     for topping in args:
-        print(f"*{topping}")
+        print(f"*{topping} blah")
 
 make_pizza('tomatoes', 'mushrooms')
--- 16_variable_arguments/16_02_kwargs.py
+++ 16_variable_arguments/16_02_kwargs.py
@@ -5,6 +5,6 @@ Write a script with a function that demonstrates the use of **kwargs.
 
 def make_pizza(**kwargs):
     for k, v in kwargs.items():
-        print(f"{k} maps to {v}")
+        print(f"{k} mapsa to {v}")
 
 make_pizza(dough='wheat', topping='tomatoes')

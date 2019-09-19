e65f46e0c2879f79d83243dc233e4557a7ac8e9e
Jesus Martinez <jesusd.martinez@Jesuss-MBP.home>
2019-09-19 15:18:43 -0700

    COMMIT 17 - DECORATORS

--- 17_decorators/17_01_wraps.py
+++ 17_decorators/17_01_wraps.py
@@ -2,3 +2,36 @@
 Write a decorator function that wraps text passed to it in a html <p> tag.
 
 '''
+
+def webby(func):
+    def wrapper(text):
+        initial = func(text)
+        new = f"<p>{initial}</p>"
+        return new
+    return wrapper
+
+@webby
+def enter_text(text):
+    return text
+
+print(enter_text('yo'))
+
+
+
+# def webby(func):
+#   def wrapper(text):
+#     initial_result = func(text)
+#     new_result = f"<p>{initial_result}</p>"
+#     return new_result
+#   return wrapper
+#
+# @webby
+# def say_something(text):
+#   return text
+#
+# print(say_something("HEI WHAT'S UP?"))
+
+
+
+
+
--- 17_decorators/17_02_tags.py
+++ 17_decorators/17_02_tags.py
@@ -3,3 +3,16 @@ Improve the decorator from the previous exercise by allowing it to take
 any specified HTML tag as an input - making it more general.
 
 '''
+
+def webby(func):
+    def wrapper(text):
+        initial = func(text)
+        new = f"<p>{initial}</p>"
+        return new
+    return wrapper
+
+@webby
+def enter_text(text):
+    return text
+
+print(enter_text('yo'))

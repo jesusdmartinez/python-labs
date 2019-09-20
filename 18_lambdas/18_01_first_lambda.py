4d314985480b8db482d49e445a671c535fbcb88c
Jesus Martinez <jesusd.martinez@Jesuss-MBP.home>
2019-09-19 17:18:42 -0700

    18 LAMBDAS COMMIT

--- 18_lambdas/18_01_first_lambda.py
+++ 18_lambdas/18_01_first_lambda.py
@@ -2,3 +2,6 @@
 Write a lambda function that takes in 3 numbers and returns the sum of the numbers.
 
 '''
+
+my_lamb = lambda x, y, z: x + y z
+print(my_lamb(3, 5, 100))
--- 18_lambdas/18_03_rewrite.py
+++ 18_lambdas/18_03_rewrite.py
@@ -4,5 +4,16 @@ Re-write the following listcomp as a lambda expression.
 '''
 
 names = ['Anne', 'Amy', 'Bob', 'David', 'Carrie', 'Barbara', 'Zach']
-
 print([x.startswith('D') for x in names])
+
+### Jesus code attempt.
+new = lambda x: "True" if x[0] == 'D' else "False", names
+print(list(new))
+
+
+
+# locations = ["indonesia", "spain", "thailand", "mexico", "usa"]
+#
+# locations.sort(key = lambda name: name[-1])
+# print(locations)
+
